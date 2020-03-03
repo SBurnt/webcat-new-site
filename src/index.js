49b5c1b420cb5f4f109696fd448f916a3d319414
SBurnt <zip2004zip@gmail.com>
2020-03-03 23:58:22 +0300

    feat: add block working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import * as $ from 'jquery'
-import './styles/main.scss'
+import * as $ from 'jquery';
+import './styles/main.scss';
 
-
+// одноременный scrollbar в блоке working
+$(function() {
+  $('.working__list-scrollbar').scroll(function() {
+    $('.working__list').scrollLeft($('.working__list-scrollbar').scrollLeft());
+  });
+  $('.working__list').scroll(function() {
+    $('.working__list-scrollbar').scrollLeft($('.working__list').scrollLeft());
+  });
+});
