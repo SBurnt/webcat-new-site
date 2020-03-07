0881439552e781f9528f56f45dbbf7def86ea81c
SBurnt <zip2004zip@gmail.com>
2020-03-08 01:00:30 +0300

    feat: add mobile menu and feedback

--- src/index.js
+++ src/index.js
@@ -10,3 +10,37 @@ $(function() {
     $('.working__list-scrollbar').scrollLeft($('.working__list').scrollLeft());
   });
 });
+
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelectorAll('.js-burger-toggle');
+const menuLink = document.querySelectorAll('.js-menu-link');
+const feedback = document.querySelector('.feedback');
+const btnFeedback = document.querySelectorAll('.js-feedback-toggle');
+// const menuBtnСhangeIcon = document.querySelector('.menu .js-burger-toggle');
+const body = document.querySelector('body');
+
+// ---------- show mobile menu start ----------
+menuBtn.forEach(items => {
+  items.addEventListener('click', item => {
+    menu.classList.toggle('active');
+    // menuBtnСhangeIcon.classList.toggle('active');
+    body.classList.toggle('overflow-hidden');
+  });
+});
+
+menuLink.forEach(items => {
+  items.addEventListener('click', item => {
+    menu.classList.toggle('active');
+    body.classList.toggle('overflow-hidden');
+  });
+});
+// ---------- show mobile menu end ----------
+
+// ---------- show feedback start ----------
+btnFeedback.forEach(items => {
+  items.addEventListener('click', item => {
+    feedback.classList.toggle('active');
+    body.classList.toggle('overflow-hidden');
+  });
+});
+// ---------- show feedback end ----------
