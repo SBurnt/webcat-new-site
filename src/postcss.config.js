a720dda9e793a5008829f78ce7c8babfcb149cdc
SBurnt <zip2004zip@gmail.com>
2020-03-09 01:43:24 +0300

    fix: img bg header

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -6,7 +6,7 @@
 module.exports = {
   plugins: [
     require('autoprefixer'),
-    require('css-mqpacker'),
+    // require('css-mqpacker'),
     require('cssnano')({
       preset: [
         'default',
