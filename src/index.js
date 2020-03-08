44a36b743fe7f641b9220bb796b1c7a62e69cad6
SBurnt <zip2004zip@gmail.com>
2020-03-08 20:09:44 +0300

    fix: add browser compatibility ie11 (forEach)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import * as $ from 'jquery';
 import './styles/main.scss';
 
+// Browser compatibility ie11 (forEach)
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function(callback, thisArg) {
+    // eslint-disable-next-line no-param-reassign
+    thisArg = thisArg || window;
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
+
 // одноременный scrollbar в блоке working
 $(function() {
   $('.working__list-scrollbar').scroll(function() {
@@ -17,13 +29,14 @@ const menuLink = document.querySelectorAll('.js-menu-link');
 const feedback = document.querySelector('.feedback');
 const btnFeedback = document.querySelectorAll('.js-feedback-toggle');
 // const menuBtnСhangeIcon = document.querySelector('.menu .js-burger-toggle');
+const html = document.querySelector('html');
 const body = document.querySelector('body');
 
 // ---------- show mobile menu start ----------
 menuBtn.forEach(items => {
   items.addEventListener('click', item => {
     menu.classList.toggle('active');
-    // menuBtnСhangeIcon.classList.toggle('active');
+    html.classList.toggle('overflow-hidden');
     body.classList.toggle('overflow-hidden');
   });
 });
@@ -31,6 +44,7 @@ menuBtn.forEach(items => {
 menuLink.forEach(items => {
   items.addEventListener('click', item => {
     menu.classList.toggle('active');
+    html.classList.toggle('overflow-hidden');
     body.classList.toggle('overflow-hidden');
   });
 });
@@ -40,6 +54,7 @@ menuLink.forEach(items => {
 btnFeedback.forEach(items => {
   items.addEventListener('click', item => {
     feedback.classList.toggle('active');
+    html.classList.toggle('overflow-hidden');
     body.classList.toggle('overflow-hidden');
   });
 });
