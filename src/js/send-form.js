880fe0c98743217cb74604d5f94e17d1afeb7437
SBurnt <zip2004zip@gmail.com>
2020-03-16 01:39:15 +0300

    feat: add feedback form validation

--- src/js/send-form.js
+++ src/js/send-form.js
@@ -1,14 +1,60 @@
+import Inputmask from 'inputmask';
+
 const btnSendFormFooter = document.querySelector('.js-send-form-footer');
 const btnSendFormFeedback = document.querySelector('.js-send-form-feedback');
 const formFooter = document.querySelector('.footer__form');
 const formFeedback = document.querySelector('.feedback__form');
 const thanks = document.querySelector('.thanks');
 const btnThanksBack = document.querySelector('.js-thanks__btn');
+const phone = document.querySelectorAll('.phone-input');
 const html = document.querySelector('html');
 const body = document.querySelector('body');
 
+Inputmask({
+  mask: '+375 (XY) 999-99-99',
+  definitions: {
+    X: {
+      validator: '2|3|4',
+      placeholder: '_',
+    },
+    Y: {
+      validator: '3|5|9|4',
+      placeholder: '_',
+    },
+  },
+}).mask(phone);
+
+const regTel = /\+\d{3} \((25|29|33|44)\) \d{3}-\d{2}-\d{2}/;
+
 btnSendFormFooter.addEventListener('click', e => {
   // e.preventDefault();
+
+  let error = 0;
+  const inputName = document.querySelector('.footer__form input[name=name]');
+  const inputTel = document.querySelector('.footer__form input[name=tel]');
+  console.log('inputName ', inputName.value);
+  console.log('inputTel ', inputTel.value);
+
+  if (inputName.value == '') {
+    error++;
+    inputName.style.border = '1px solid #ff0000';
+  } else {
+    inputName.style.border = 'none';
+    inputName.style.borderBottom = '1px solid #ce0476';
+  }
+
+  if (regTel.exec(inputTel.value) == null) {
+    error++;
+    inputTel.style.border = '1px solid #ff0000';
+  } else {
+    inputTel.style.border = 'none';
+    inputTel.style.borderBottom = '1px solid #ce0476';
+  }
+
+  if (error > 0) {
+    return false;
+  }
+
   btnSendFormFooter.disabled = true;
   let formData = new FormData(formFooter);
 
@@ -27,6 +73,33 @@ btnSendFormFooter.addEventListener('click', e => {
 
 btnSendFormFeedback.addEventListener('click', e => {
   // e.preventDefault();
+
+  let error = 0;
+  const inputName = document.querySelector('.feedback__form input[name=name]');
+  const inputTel = document.querySelector('.feedback__form input[name=tel]');
+  console.log('inputName ', inputName.value);
+  console.log('inputTel ', inputTel.value);
+
+  if (inputName.value == '') {
+    error++;
+    inputName.style.border = '1px solid #ff0000';
+  } else {
+    inputName.style.border = 'none';
+    inputName.style.borderBottom = '1px solid #ce0476';
+  }
+
+  if (regTel.exec(inputTel.value) == null) {
+    error++;
+    inputTel.style.border = '1px solid #ff0000';
+  } else {
+    inputTel.style.border = 'none';
+    inputTel.style.borderBottom = '1px solid #ce0476';
+  }
+
+  if (error > 0) {
+    return false;
+  }
+
   btnSendFormFeedback.disabled = true;
   let formData = new FormData(formFeedback);
 
