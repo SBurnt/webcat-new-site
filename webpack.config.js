b6e81e5cd4f6541500e285560ab21f1d6483d890
SBurnt <zip2004zip@gmail.com>
2020-04-04 20:36:15 +0300

    add bg color

--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,9 @@ const optimizationImg = () => {
         gifsicle: {
           interlaced: false,
         },
+        // webp: {
+        //   quality: 90
+        // },
       },
     };
   }
@@ -175,7 +178,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(gif|png|jpe?g|svg)$/i,
+        test: /\.(gif|png|jpe?g|svg|webp)$/i,
         use: [
           {
             loader: 'file-loader',
